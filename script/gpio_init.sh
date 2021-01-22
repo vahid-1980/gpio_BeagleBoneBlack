ed91d574592572eedb483b867904b8177a6072b3
Vahid Gharaee <vgharaee@gmail.com>
2021-01-22 14:11:44 +0330

    modified the gpio_init script. tested on beaglebobe black
    Signed-off-by: Vahid Gharaee <vgharaee@gmail.com>

--- script/gpio_init.sh
+++ script/gpio_init.sh
@@ -1,40 +1,36 @@
 #!/bin/bash
+# usage: source gpio_init.sh <input gpio num> <output gpio num>
+#it will export gpios and set their directions for those argumants, in sysfs.
 
-gpio_dir="/sys/class/gpio"
-
-gpio_in="/sys/class/gpio/gpio$1"
-gpio_out="/sys/class/gpio/gpio$2"
-
-gpio_in_direction="${gpio_in}/direction"
-gpio_out_direction="${gpio_out}/direction"
-
-gpio_in_value="${gpio_in}/value"
-gpio_out_value="${gpio_out}/value"
-
-
-if [ ! -d $gpio_in ]; then 
-    echo $1 > ${gpio_dir}/export
-    if [ ! $? -eq 0 ]; then
-        echo "something's wrong with gpio $1"
-        exit -1
-    else
-        echo "in" > $gpio_in_direction
-    fi
-else
-    echo "in" > $gpio_in_direction
+if [ -d "/sys/class/gpio/gpio"$1 ]; then 
+	echo $1 > /sys/class/gpio/unexport
+	sleep 0.01
 fi
 
-
-if [ ! -d $gpio_out ]; then 
-    echo $2 > ${gpio_dir}/export
-    if [ ! $? -eq 0 ]; then
-        echo "something's wrong with gpio $2"
-        exit -1
-    else
-        echo "out" > $gpio_in_direction
-    fi
-else
-    echo "out" > $gpio_in_direction
+if [ -d "/sys/class/gpio/gpio"$2 ]; then
+	echo $2 > /sys/class/gpio/unexport
+	sleep 0.01
 fi
 
-echo 0 > $gpio_out_value
+echo $1 > /sys/class/gpio/export
+sleep 0.1
+
+echo $2 > /sys/class/gpio/export
+sleep 0.1
+
+if [ -d "/sys/class/gpio/gpio"$1 ]; then
+	echo "in" > /sys/class/gpio/gpio$1/direction
+else
+	echo "something's wrong with gpio $1"
+	exit -1
+fi
+
+if [ -d "/sys/class/gpio/gpio"$2 ]; then
+	echo "out" > /sys/class/gpio/gpio$2/direction && 
+	echo "1" > /sys/class/gpio/gpio$2/value
+else
+	echo "something's wrong with gpio $2"
+	exit -1
+fi
+
+echo 0 > /sys/class/gpio/gpio$2/value
